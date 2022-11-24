a47e9b3b2687478f94061a0948ac973d7370b379
kjyook <kjyook01@gmail.com>
2022-11-24 17:43:00 +0900

    how to move character at main

--- gameProject/main.py
+++ gameProject/main.py
@@ -2,11 +2,30 @@ from PIL import Image, ImageDraw, ImageFont
 import time
 import random
 from colorsys import hsv_to_rgb
-
 from Joystick import Joystick
 
 def main():
     joystick = Joystick()
+    my_character = Image.open() #Todo! -> upload png file
+
+    while True:
+        command = {'move': False, 'up_pressed': False , 'down_pressed': False, 'left_pressed': False, 'right_pressed': False}
+        
+        if not joystick.button_U.value:  # up pressed
+            command['up_pressed'] = True
+            command['move'] = True
+
+        if not joystick.button_D.value:  # down pressed
+            command['down_pressed'] = True
+            command['move'] = True
+
+        if not joystick.button_L.value:  # left pressed
+            command['left_pressed'] = True
+            command['move'] = True
+
+        if not joystick.button_R.value:  # right pressed
+            command['right_pressed'] = True
+            command['move'] = True
 
 
 if __name__ == '__main__':
