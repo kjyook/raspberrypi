3f62c28110f2100fa08fea9df16251202443808a
kjyook <kjyook01@gmail.com>
2022-11-24 16:54:50 +0900

    11-24 게임 만들기 시작

---
+++ gameProject/Character.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+class Character:
+    def __init__(self, width, height):
+        self.appearance = 'circle'
+        self.state = None
---
+++ gameProject/main.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw, ImageFont
+import time
+import random
+from colorsys import hsv_to_rgb
+
+from Joystick import Joystick
+
+def main():
+    joystick = Joystick()
+
+
+if __name__ == '__main__':
+    main()
