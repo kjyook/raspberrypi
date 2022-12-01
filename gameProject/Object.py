ae38a74765c0c369d4524570d78c8abe1eff4936
kjyook <kjyook01@gmail.com>
2022-12-01 17:46:22 +0900

    캐릭터 상하 이동 + 장애물 굴러오기  구현 완료

--- gameProject/Object.py
+++ gameProject/Object.py
@@ -4,8 +4,11 @@ class Object:
     """super class"""
     
     def __init__(self, spawn_position):
+        self.appearance = 'rectangle'
         self.position = np.array([spawn_position[0] - 25, spawn_position[1] - 25, spawn_position[0] + 25, spawn_position[1] + 25])
         self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
+        self.state = None
+        self.outline = "#0000FF"
 
     def move(self, command = None):
         if command['move'] == False:
