9ff59cff2e30737ad8bb99f02f70f1797c0ede63
kjyook <kjyook01@gmail.com>
2023-09-19 14:05:05 +0900

    Update Obstacle.py

--- gameProject/Obstacle.py
+++ gameProject/Obstacle.py
@@ -3,12 +3,28 @@ import time
 
 class Obstacle:
     def __init__(self, spawn_position):
-        self.appearance = 'circle'
         self.state = 'alive'
         self.position = np.array([spawn_position[0] - 25, spawn_position[1] - 25, spawn_position[0] + 25, spawn_position[1] + 25])
-        self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
-        self.outline = "#00FF00"
+        self.speed = 3
 
-    def move(self):
-            self.position[0] -= 5
-            self.position[2] -= 5
+    def move(self, command = None):
+        if command['move'] != False:
+            if command['left_pressed']:
+                self.speed = 2
+            
+            if command['right_pressed']:
+                self.speed = 4
+
+        self.position[0] -= self.speed
+        self.position[2] -= self.speed
+
+    def collision_check(self, character):
+        collision = self.overlap(self.position, character.position)
+
+        if collision:
+            character.alive = 'die'
+            self.state = 'hit'
+
+    def overlap(self, enemy_position, character_position):
+        return character_position[0] > enemy_position[0] and character_position[1] > enemy_position[1] \
+            and character_position[2] < enemy_position[2] and character_position[3] < enemy_position[3]
